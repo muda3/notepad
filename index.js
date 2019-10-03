d507988c2b38b7082efa35b06a8f6fd6abe553be
muda3 <yunepehe@f5.si>
2019-10-03 18:01:06 +0900

    color add

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const { app, BrowserWindow, systemPreferences } = require('electron')
-let win
+const { app, BrowserWindow, systemPreferences } = require("electron");
+let win;
 function createWindow() {
-    win = new BrowserWindow({
-        width: 890,
-        height: 1000,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-    systemPreferences.subscribeNotification(
-        'AppleInterfaceThemeChangedNotification',
-        function theThemeHasChanged() {
-            updateMyAppTheme(systemPreferences.isDarkMode())
-        }
-    )
-    win.loadFile('index.html')
-    win.webContents.openDevTools()
-    win.on('closed', () => {
-        win = null
-    })
+  win = new BrowserWindow({
+    width: 890,
+    height: 1000,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  systemPreferences.subscribeNotification(
+    "AppleInterfaceThemeChangedNotification",
+    function theThemeHasChanged() {
+      updateMyAppTheme(systemPreferences.isDarkMode());
+    }
+  );
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
-app.on('ready', createWindow)
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
-app.on('activate', () => {
-    if (win === null) {
-        createWindow()
-    }
-})
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
