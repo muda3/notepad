74550fc9a58a45c60304a0872845a5dc2924e3d7
muda <yunepehe@f5.si>
2019-10-18 22:58:56 +0900

    delete menu item

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, systemPreferences, Menu,isMac } = require("electron");
+const { app, BrowserWindow, systemPreferences, Menu,isMac,shell } = require("electron");
 let win;
 const join = require('path').join;
 const openAboutWindow = require('about-window').default;
@@ -46,7 +46,6 @@ const template = [
             copyright: 'Copyright (c) 2019 muda3',
           })},
       { type: 'separator' },
-      { role: 'services' },
       { type: 'separator' },
       { role: 'hide' },
       { role: 'hideothers' },
@@ -55,81 +54,13 @@ const template = [
       { role: 'quit' }
     ]
   }] : []),
-  // { role: 'fileMenu' }
-  {
-    label: 'File',
-    submenu: [
-      isMac ? { role: 'close' } : { role: 'quit' }
-    ]
-  },
-  // { role: 'editMenu' }
-  {
-    label: 'Edit',
-    submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      ...(isMac ? [
-        { role: 'pasteAndMatchStyle' },
-        { role: 'delete' },
-        { role: 'selectAll' },
-        { type: 'separator' },
-        {
-          label: 'Speech',
-          submenu: [
-            { role: 'startspeaking' },
-            { role: 'stopspeaking' }
-          ]
-        }
-      ] : [
-        { role: 'delete' },
-        { type: 'separator' },
-        { role: 'selectAll' }
-      ])
-    ]
-  },
-  // { role: 'viewMenu' }
-  {
-    label: 'View',
-    submenu: [
-      { role: 'reload' },
-      { role: 'forcereload' },
-      { role: 'toggledevtools' },
-      { type: 'separator' },
-      { role: 'resetzoom' },
-      { role: 'zoomin' },
-      { role: 'zoomout' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
-  },
-  // { role: 'windowMenu' }
-  {
-    label: 'Window',
-    submenu: [
-      { role: 'minimize' },
-      { role: 'zoom' },
-      ...(isMac ? [
-        { type: 'separator' },
-        { role: 'front' },
-        { type: 'separator' },
-        { role: 'window' }
-      ] : [
-        { role: 'close' }
-      ])
-    ]
-  },
   {
     role: 'help',
     submenu: [
       {
         label: 'Learn More',
         click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://electronjs.org')
+          await shell.openExternal('https://github.com/muda3/notepad')
         }
       }
     ]
